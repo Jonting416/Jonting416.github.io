51d31e26c0da26cfd9cd0aea28a52b5348b5f107
Jon Ting <jting41@gatech.edu>
2025-09-22 20:03:10 -0700

    Update crossword

--- crossword/src/App.jsx
+++ crossword/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function EmbedCrosshare() {
     return <iframe style={{height: "90vh", width: "100%"}}
-                   src="https://crosshare.org/embed/XT0vthMZCyMRHWAD6oVu/IsflylxT9BXFFZbykdgp6PI7lyt2" frameBorder="0"
+                   src="https://crosshare.org/embed/Mcm2gKtHj4WdYkBlufFA/IsflylxT9BXFFZbykdgp6PI7lyt2" frameBorder="0"
                    allowFullScreen="true" allowTransparency="true" allow="clipboard-write *"></iframe>;
 }
 
