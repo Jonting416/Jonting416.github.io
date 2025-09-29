1dc18e50113ca1965b05b8a07f41a0d3dd814e31
Jon Ting <jting41@gatech.edu>
2025-09-28 23:26:31 -0700

    Shrink crosshare height

--- crossword/src/App.jsx
+++ crossword/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function EmbedCrosshare() {
-    return <iframe style={{height: "90vh", width: "100%"}}
+    return <iframe style={{height: "85vh", width: "100%"}}
                    src="https://crosshare.org/embed/Mcm2gKtHj4WdYkBlufFA/IsflylxT9BXFFZbykdgp6PI7lyt2" frameBorder="0"
                    allowFullScreen="true" allowTransparency="true" allow="clipboard-write *"></iframe>;
 }
