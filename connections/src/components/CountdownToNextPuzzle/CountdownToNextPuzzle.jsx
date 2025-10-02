015abf641c59ef850dc98fe94235dd755ecbb63c
Jon Ting <jting41@gatech.edu>
2025-10-02 00:04:16 -0700

    Revert clearLocalStorage button in results modal

--- connections/src/components/CountdownToNextPuzzle/CountdownToNextPuzzle.jsx
+++ connections/src/components/CountdownToNextPuzzle/CountdownToNextPuzzle.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { clearLocalStorage } from "../../lib/local-storage.jsx";
-import { Button } from "../ui/button";
-
 
 // Yes this is called CountdownToNextPuzzle but for our purposes we aren't time refreshing the puzzles.
 function CountdownToNextPuzzle() {
   return (
     <div className="flex flex-row place-content-center mt-4">
-      <Button variant="link" onClick={clearLocalStorage()}>Reset Puzzle Status</Button>
+      <div>Try the other puzzle to reset this puzzle!</div>
     </div>
   );
 }
