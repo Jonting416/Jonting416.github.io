9adf57be4823e6c9f877549ae172a7bec877f0bf
Jon Ting <jting416@gmail.com>
2025-10-22 00:41:15 -0400

    Add header bar to crossword

--- crossword/src/App.jsx
+++ crossword/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import "./styles/App.scss";
 
 function EmbedCrosshare() {
-    return <iframe style={{height: "85vh", width: "100%"}}
-                   src="https://crosshare.org/embed/Mcm2gKtHj4WdYkBlufFA/IsflylxT9BXFFZbykdgp6PI7lyt2" frameBorder="0"
-                   allowFullScreen="true" allowTransparency="true" allow="clipboard-write *"></iframe>;
+    return (
+      <div className="crossword">
+        <header className="header-bar">
+          <a href="../index.html">
+            <img className="logo" href="index.html" src="/data/jtgameslogoinverted.svg" alt="???"/>
+          </a>
+        </header>
+        <iframe style={{height: "85vh", width: "100%", "margin-top": "60px"}}
+          src="https://crosshare.org/embed/Mcm2gKtHj4WdYkBlufFA/IsflylxT9BXFFZbykdgp6PI7lyt2" frameBorder="0"
+          allowFullScreen="true" allowTransparency="true" allow="clipboard-write *">
+        </iframe>
+      </div>
+    );
 }
 
 export default EmbedCrosshare;
